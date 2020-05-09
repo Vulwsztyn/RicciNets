7fd3f0b61fc123ac8eb2e238bf4a0aec4e97b56c
seglass5 <s.glass.cls@gmail.com>
2020-05-09 12:48:19 +0100

    Comment on function

--- isolated_nodes.py
+++ isolated_nodes.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+# Function to return the list of nodes in a graph with no connected edges
 def get_isolated_nodes(directed_graph):
     isolated_nodes = []
     for node in list(directed_graph.nodes()):
